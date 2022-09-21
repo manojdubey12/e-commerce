42092633f2f6720e1e4d977bc2fead74c5b116dc
manojdubey12 <manojduebyatoz@gmail.com>
2022-09-21 19:51:30 +0530

    third

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Product
+from math import ceil
+
 def index(request):
-    return render(request,"shop/index.html")
+    products= Product.objects.all()
+    allprods= []
+    # allprods= [[products, range(1, nslides), nslides],[products, range(1, nslides), nslides]]
+    # print(allprods)
+    # params = {"allprods": allprods}
+    catprods = Product.objects.values('category', 'id')
+    cats={item['category'] for item in catprods}
+    for cat in cats:
+        prod= Product.objects.filter(category=cat)
+        n= len(products)
+        nslides = n//4 + ceil((n/4) + (n//4))
+        allprods.append([prod,range(1, nslides),nslides])
+
+    params = {"allprods": allprods}
+    #params ={'nslides': nslides, 'range': range(1, nslides), 'products': products}
+    return render(request,"shop/index.html",params)
 
 def about(request):
-    return HttpResponse("shop about")
+    return render(request,"shop/about.html")
 
 def contact(request):
     return HttpResponse("shop contact")
