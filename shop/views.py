b90e675f764760a1a7e3202730ddb25b7f02d49a
manojdubey12 <manojduebyatoz@gmail.com>
2022-09-23 19:20:48 +0530

    five

--- shop/views.py
+++ shop/views.py
@@ -4,23 +4,43 @@ from django.http import HttpResponse
 from .models import Product
 from math import ceil, prod
 
-def index(request):
-    products= Product.objects.all()
-    allprods= []
-    # allprods= [[products, range(1, nslides), nslides],[products, range(1, nslides), nslides]]
-    # print(allprods)
-    # params = {"allprods": allprods}
-    catprods = Product.objects.values('category', 'id')
-    cats={item['category'] for item in catprods}
-    for cat in cats:
-        prod= Product.objects.filter(category=cat)
-        n= len(products)
-        nslides = n//4 + ceil((n/4) + (n//4))
-        allprods.append([prod,range(1, nslides),nslides])
+# def index(request):
+#     allProds= []
+#     # allprods= [[products, range(1, nslides), nslides],[products, range(1, nslides), nslides]]
+#     # print(allprods)
+#     # params = {"allprods": allprods}
+#     catprods = Product.objects.values('category', 'id')
+#     cats={item['category'] for item in catprods}
+#     for cat in cats:
+#         prod= Product.objects.filter(category=cat)
+#         n= len(prod)
+#         nSlides = n//4 + ceil((n/4) - (n//4))
+#         allProds.append([prod,range(1, nSlides),nSlides])
 
-    params = {"allprods": allprods}
-    #params ={'nslides': nslides, 'range': range(1, nslides), 'products': products}
-    return render(request,"shop/index.html",params)
+#     params = {"allProds": allProds}
+#     #params ={'nslides': nslides, 'range': range(1, nslides), 'products': products}
+#     return render(request,"shop/index.html",params)
+
+def index(request):
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return render(request,"shop/about.html")
